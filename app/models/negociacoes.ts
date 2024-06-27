a5945eeabe9ab0c852fb50a3d2803d1b4fd84301
Nick-Rodrigues <RM551060@fiap.com.br>
2024-06-27 11:36:02 -0300

    Otimização para recursos de typescript.

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -1,12 +1,14 @@
 import { Negociacao } from "./negociacao.js";
 
 export class Negociacoes {
+    // private negociacoes: Negociacao[] = [];
     private negociacoes: Array<Negociacao> = [];
 
     adiciona(negociacao: Negociacao) {
         this.negociacoes.push(negociacao)
     }
 
+    // lista(): readonly Negociacao[]
     lista(): ReadonlyArray<Negociacao>{
         return this.negociacoes;
     }
--- dist/js/models/negociacoes.js
+++ dist/js/models/negociacoes.js
@@ -1,10 +1,12 @@
 export class Negociacoes {
     constructor() {
+        // private negociacoes: Negociacao[] = [];
         this.negociacoes = [];
     }
     adiciona(negociacao) {
         this.negociacoes.push(negociacao);
     }
+    // lista(): readonly Negociacao[]
     lista() {
         return this.negociacoes;
     }
